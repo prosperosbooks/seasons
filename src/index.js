71e552d67a21fdf2de01f1751b7d652d9f16d851
Emin <allen.emin@gmail.com>
2019-01-20 03:07:53 -0500

    set up API to fetch location

--- src/index.js
+++ src/index.js
@@ -3,15 +3,44 @@ import ReactDOM from "react-dom";
 import SeasonDisplay from "./SeasonDisplay";
 
 // determines location + month
-
 class App extends React.Component {
-  render() {
-    window.navigator.geolocation.getCurrentPosition(
-      position => console.log(position),
-      err => console.log(err)
-    );
+  state = { lat: null, long: null, errorMessage: "" };
 
-    return <div>Latitude:</div>;
+  componentDidMount() {
+    console.log("My component was rendered");
+    window.navigator.geolocation.getCurrentPosition(
+      position => {
+        console.log(position);
+        this.setState({
+          lat: position.coords.latitude,
+          long: position.coords.longitude,
+          errorMessage: ""
+        });
+      },
+      err => {
+        this.setState({ errorMesssage: err.message });
+      }
+    );
+  }
+
+  componentDidUpdate() {
+    console.log("My component was updated!");
+  }
+
+  render() {
+    if (this.state.errorMessage && !this.state.lat) {
+      return <div>Error: {this.state.errorMessage} </div>;
+    }
+
+    if (!this.state.errorMessage && this.state.long) {
+      return <div>Longitude: {this.state.long}</div>;
+    }
+
+    if (!this.state.errorMessage && this.state.lat) {
+      return <div>Latitude: {this.state.lat}</div>;
+    }
+
+    return <div>Loading!</div>;
   }
 }
 
